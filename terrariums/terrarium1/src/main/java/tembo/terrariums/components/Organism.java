ddec22a5f35bcad1b985a7c5a0df5060c3ea619e
wingutembo@gmail.com <mike.angel@ngc.com>
2016-02-26 20:43:10 -0800

    Organism

--- terrariums/terrarium1/src/main/java/tembo/terrariums/components/Organism.java
+++ terrariums/terrarium1/src/main/java/tembo/terrariums/components/Organism.java
@@ -18,10 +18,13 @@ public class Organism
 	private static int idfactory = 1;
 	private int id = idfactory++;
 	
+	private int age = 0;
+	
 	/**
 	 * Constructor
 	 * @param sizeMax
 	 * @param energyMax
+	 * @param initialSize
 	 */
 	public Organism(double sizeMax, double energyMax, double initialSize)
 	{
@@ -64,7 +67,7 @@ public class Organism
 	 * @return the energy required for metabolism at current size
 	 */
 	
-	private double metabolismAtCurrentSize()
+	protected double metabolismAtCurrentSize()
 	{
 		double y = Math.exp(K*currentSize);
 		return y;
@@ -89,6 +92,8 @@ public class Organism
 	 */
 	protected void grow(final double energyAbsorbed) throws OrganismDeadException
 	{
+		age++;
+		
 		/* compute where in the energy to size profile it falls */
 		double minEnergy = metabolismAtCurrentSize();
 		if(energyAbsorbed<minEnergy)
@@ -113,13 +118,20 @@ public class Organism
 			// Grow the organism with excess energy
 			double excessEnergy = actualEnergyAbsorbed - minEnergy;
 			this.currentSize += excessEnergy * Universe.sizeToEnergyConstant;
+			
+			// Organism won't croak, this is a hack. , the excess energy gets too small 
+			double percent = this.currentSize/sizeMax * 100.0;
+			if(percent>99.0)
+			{
+				throw new OrganismDeadException("Organism "+this.id+" died at age "+this.age);
+			}
 		}
 		
-		if(logger.isTraceEnabled())
+ 		if(logger.isTraceEnabled())
 		{
 			logger.trace(this);
 		}
-	}
+ 	}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
@@ -155,7 +167,120 @@ public class Organism
 	@Override
 	public String toString() {
 		return "Organism [sizeMax=" + sizeMax + ", energyMax=" + energyMax
-				+ ", currentSize=" + currentSize + ", id=" + id + "]";
+				+ ", currentSize=" + currentSize + ", K=" + K + ", id=" + id
+				+ ", age=" + age + "]";
+	}
+
+	/**
+	 * @return the logger
+	 */
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	/**
+	 * @param logger the logger to set
+	 */
+	public static void setLogger(Logger logger) {
+		Organism.logger = logger;
+	}
+
+	/**
+	 * @return the sizeMax
+	 */
+	public double getSizeMax() {
+		return sizeMax;
+	}
+
+	/**
+	 * @param sizeMax the sizeMax to set
+	 */
+	public void setSizeMax(double sizeMax) {
+		this.sizeMax = sizeMax;
+	}
+
+	/**
+	 * @return the energyMax
+	 */
+	public double getEnergyMax() {
+		return energyMax;
+	}
+
+	/**
+	 * @param energyMax the energyMax to set
+	 */
+	public void setEnergyMax(double energyMax) {
+		this.energyMax = energyMax;
+	}
+
+	/**
+	 * @return the currentSize
+	 */
+	public double getCurrentSize() {
+		return currentSize;
+	}
+
+	/**
+	 * @param currentSize the currentSize to set
+	 */
+	public void setCurrentSize(double currentSize) {
+		this.currentSize = currentSize;
+	}
+
+	/**
+	 * @return the k
+	 */
+	public double getK() {
+		return K;
+	}
+
+	/**
+	 * @param k the k to set
+	 */
+	public void setK(double k) {
+		K = k;
+	}
+
+	/**
+	 * @return the idfactory
+	 */
+	public static int getIdfactory() {
+		return idfactory;
+	}
+
+	/**
+	 * @param idfactory the idfactory to set
+	 */
+	public static void setIdfactory(int idfactory) {
+		Organism.idfactory = idfactory;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
 	}
 	
 }
